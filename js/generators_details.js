f08438d631204b1b87743ae190de2deb0aa8af6f
nice-shot <shefferu@gmail.com>
2014-12-19 03:43:10 +0200

    added dynamic generators. Too bad loading using json is giving me trouble...

---
+++ js/generators_details.js
@@ -0,0 +1,10 @@
+var generatorsDetails = [
+	{
+		id: "Voter",
+		name: "מצביע",
+		description: "יצביע בשבילך כל 5 שניות לא משנה מה. כמו אנשים אחרים שאתה מכיר",
+		price: 10,
+		picture: "loneVoter.png",
+		summary: "הצבעה כל 5 שניות",
+	}
+]
--- js/vote.js
+++ js/vote.js
@@ -8,8 +8,63 @@ Copyright 2014 or something...
 Version 0.1.0
 */
 
-function Generator() {
+function Generator(generatorsDiv, details) {
+	/*
+	generatorsDiv - jQuery of the generators container div ( $("#geneartors") )
+	details - an object with the necessary details about this generator.
+			  Example:
+			  {
+			 	id: "Voter",
+			 	name: "voter",
+			 	description: "He'll always vote for you!",
+			 	price: 5,
+			 	picture: "voter.png",
+			 	summary: "Adds +5 every second",
+		 	  }
+	*/
+	console.log("started creating generator")
+	
+
+	/* Creates the html element for the generator */
+	this.init = function() {
+		var btnElem = document.createElement("div");
+		var imgElem = document.createElement("img");
+		var priceElem = document.createElement("div");
+		var descElem = document.createElement("div");
+		var levelElem = document.createElement("div");
+		var summaryElem = document.createElement("div");
+		var summaryTextElem = document.createElement("p");
+
+		btnElem.id = "gen" + details.id;
+		btnElem.className = "genBtn";
+
+		imgElem.src = "assets/" + details.picture;
+		imgElem.className = "genBtnPic";
+
+		priceElem.className = "genBtnPrice";
+		priceElem.textContent = details.name + " - " + details.price + "₪";
+
+		descElem.className = "genBtnDesc";
+		descElem.textContent = details.description;
+
+		levelElem.className = "genBtnLvl";
+		levelElem.textContent = 0;
+
+		summaryElem.className = "genBtnSummary";
+		summaryTextElem.textContent = details.summary;
+		summaryElem.appendChild(summaryTextElem);
+
+		var jqBtn = $(btnElem);
+		jqBtn.append([imgElem, priceElem, descElem, levelElem, summaryElem]);
+		generatorsDiv.append(jqBtn);
+		return jqBtn;
+	}
+
+	this.btn = this.init()
+
+	console.log("finished creating generator")
 };
+
 
 function Game() {
 	"use strict";
@@ -17,6 +72,7 @@ function Game() {
 	var votes = 0;
 	var votesPerSecond = 0;
 	var votesClickValue = 1;
+	var generators = []
 
 	var displayVotes = function() {
 		$("#votesNumText").text(votes)
@@ -25,11 +81,23 @@ function Game() {
 		votes += votesClickValue;
 		displayVotes();
 	}
+	
 
 	this.reset = function() {
 		console.log("reseting");
+
 		// Attach click event to vote note
-		$("#noteImg").on("click", clickEvent)
+		$("#noteImg").on("click", clickEvent);
+
+		// Create generators:
+		var gensDiv = $("#generators");
+		generatorsDetails.forEach(function (item) {
+			generators.push(new Generator(gensDiv, item));
+		});
+		//$.getJSON("js/generators.json", function(data) {
+		//	console.log(data)
+		//});
+
 	}
 
 	this.start = function() {
