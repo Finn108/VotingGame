e301ff6e1ef3dce3465bbae23d2673749d4fb8e8
nice-shot <shefferu@gmail.com>
2014-12-20 14:01:20 +0200

    added the upgrades_details file that contains the different upgrades

---
+++ js/upgrades_details.js
@@ -0,0 +1,24 @@
+var upgradesDetails = [
+	{
+		id: "ClickPlus1",
+		name: "שדרוג קליק",
+		price: 10,
+		description: "כל קליק שווה פי 2",
+		func: function(game) {
+			game.votesClickValue ++;
+		},
+	},
+	{
+		id: "VoterEachSecond",
+		name: "שדרוג מצביע",
+		price: 9,
+		description: "מצביעים עכשיו הולכים לקלפי כל שנייה",
+		func: function (game) {
+			var voterGenerator = $.grep(game.generators, function(item) { 
+				return item.id === "Voter";
+			})[0];
+ 
+			voterGenerator.updateVotesPerSecond(1);
+		},
+	},
+]
