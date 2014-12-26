dab7b30ab489c0653b2ac57d74c5b33174ca3a1f
nice-shot <shefferu@gmail.com>
2014-12-26 15:19:13 +0200

    added hints to the opening sequence

--- src/opening_sequence.js
+++ src/opening_sequence.js
@@ -7,6 +7,9 @@ function openingSequence() {
 	var note = $("#note");
 	var noteTitle = note.children().first();
 	var noteDesc = note.children().last();
+	var titleHintTimeout;
+	var descHintTimeout;
+	var clickHintTimeout;
 	
 	function fadeNoteIn() {
 		/*
@@ -15,12 +18,34 @@ function openingSequence() {
 		note.fadeIn();
 		setTimeout(function() {
 			noteTitle.focus();
+			titleHintTimeout = setTimeout(addTitleHint, 2000);
 		}, 1000);
+	}
+	
+	function addHintClick() {
+		$("#noteVoteHint").fadeIn();
+	}
+	
+	function addTitleHint() {
+		$("#noteTitleHint").fadeIn();
+	}
+	
+	function addDescHint() {
+		$("#noteSubtitleHint").fadeIn();
+	}
+	
+	function clearClickHint() {
+		clearTimeout(clickHintTimeout);
+		$("#noteVoteHint").fadeOut();
+		note.off("click", clearClickHint);
 	}
 	
 	// Change the enter key for the note title
 	noteTitle.keydown(function (event) {
 		if (event.which === 13) {
+			clearTimeout(titleHintTimeout);
+			$("#noteTitleHint").fadeOut();
+			descHintTimeout = setTimeout(addDescHint, 2000);
 			this.readOnly = true;
 			$(this).disableSelection();
 			noteDesc.focus();
@@ -31,10 +56,14 @@ function openingSequence() {
 	// Change the enter key for the note description
 	noteDesc.keydown(function (event) {
 		if (event.which === 13) {
+			clearTimeout(descHintTimeout);
+			$("#noteSubtitleHint").fadeOut();
 			this.readOnly = true;
 			$(this).disableSelection();
 			event.preventDefault();
 			note.addClass("anim tilt");
+			clickHintTimeout = setTimeout(addHintClick, 2000);
+			note.on("click", clearClickHint);
 		}
 	});
 	
