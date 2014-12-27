a3c38da3a3f44b1d28f3c6ba050d770943cbdeac
nice-shot <shefferu@gmail.com>
2014-12-27 11:02:26 +0200

    added query string reading to the play script so we can easily skip the intro

--- src/play.js
+++ src/play.js
@@ -4,6 +4,32 @@ Starts the game
 
 $(document).ready(function() {
 	var game = new Game();
-	game.reset(true);
+	var queryString = (function () {
+	  // This function is anonymous, is executed immediately and
+	  // the return value is assigned to QueryString!
+	  var qs = {};
+	  var query = window.location.search.substring(1);
+	  var vars = query.split("&");
+	  for (var i=0; i < vars.length; i++) {
+	    var pair = vars[i].split("=");
+	    	// If first entry with this name
+	    if (typeof qs[pair[0]] === "undefined") {
+	      qs[pair[0]] = pair[1];
+	    	// If second entry with this name
+	    } else if (typeof qs[pair[0]] === "string") {
+	      var arr = [ qs[pair[0]], pair[1] ];
+	      qs[pair[0]] = arr;
+	    	// If third or later entry with this name
+	    } else {
+	      qs[pair[0]].push(pair[1]);
+	    }
+	 }
+	    return qs;
+	})();
+
+	console.log(queryString);
+
+	skipIntro = "skipIntro" in queryString;
+	game.reset(skipIntro);
 	game.start();
 });
