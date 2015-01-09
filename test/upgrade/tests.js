6a4e6293fe3901d202b09064e91a5602d0e3148f
nice-shot <shefferu@gmail.com>
2015-01-09 19:01:18 +0200

    added a test for the multiple upgrade buy bug

--- test/upgrade/tests.js
+++ test/upgrade/tests.js
@@ -8,6 +8,15 @@ var upgradesDetails = [
     func: function (game) {
       game.votesCounter.addVotes(1);
     },
+  },
+  {
+    id: "Upg2",
+    name: "Click is worth +2",
+    description: "",
+    price: 5,
+    func: function (game) {
+      game.votesClickValue += 2;
+    }
   }
 ];
 
@@ -25,4 +34,26 @@ QUnit.test("buy basic upgrade", function(assert) {
     "1",
     "Upgrade should leave one point"
   );
+});
+
+QUnit.test("can't buy more than once", function(assert) {
+  var game = new Game();
+  game.reset(true, 20);
+  var votesDisplay = $("#votesNumText");
+  var note = $("#note");
+
+  note.click();
+  assert.equal(votesDisplay.text(), "21", "One click is one vote");
+
+  var myUp = game.getUpgById("Upg2");
+  myUp.display();
+
+  for (var i=0; i < 4; i++) {
+    $("#upgradeUpg2").click();
+  }
+
+  assert.equal(votesDisplay.text(), "16", "Upgrade is only bought once");
+  note.click();
+  assert.equal(votesDisplay.text(), "19", "One click is three vote");
+
 });
